c350947a2e5227d23133c8ef634dfe9713d29b75
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-12 05:31:01 -0400

    created SeasonDisplay component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 const App = () => {
   return (
     <div>
+      <SeasonDisplay />
       <h1>Hello there!</h1>
     </div>
   );
 };
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
