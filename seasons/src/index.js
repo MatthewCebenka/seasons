1fe3b0c3a7300983ddd1b029e814b58aa6e11e22
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-13 07:03:16 -0400

    added component did mount function

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -7,35 +7,30 @@ class App extends React.Component {
     super(props);
 
     this.state = { lat: null, errorMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // we called set.State to update state of the lat object.
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
   render() {
-    if(this.state.errorMessage && !this.state.lat){
-      return <div>Error: {this.state.errorMessage}</div>
-    } if (!this.state.errormessage && this.state.lat){
-      return <div>Lat: {this.state.lat}</div>
-    } else {
-      return <div>Loading!</div>
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
     }
-    
-    
-    
-    
-      // <div>
-      //   Latitude: {this.state.lat}
-      //   <br />
-      //   Error: {this.state.errorMessage}
-      // </div>
+    if (!this.state.errormessage && this.state.lat) {
+      return <div>Lat: {this.state.lat}</div>;
+    } else {
+      return <div>Loading!</div>;
+    }
+
+    // <div>
+    //   Latitude: {this.state.lat}
+    //   <br />
+    //   Error: {this.state.errorMessage}
+    // </div>
   }
 }
 
